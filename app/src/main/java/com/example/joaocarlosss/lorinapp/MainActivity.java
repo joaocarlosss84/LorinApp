93e2b436a3804012089e8b3cbb33333e7f3caba0
joaocarlosss84 <joaocarlosss@gmail.com>
2017-04-28 19:55:31 -0300

    Added onChange TimePicker

--- app/src/main/java/com/example/joaocarlosss/lorinapp/MainActivity.java
+++ app/src/main/java/com/example/joaocarlosss/lorinapp/MainActivity.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int iCurrentTab;
+    private TabHost tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final TabHost tabs = (TabHost) findViewById(R.id.tabHost);
+
+        tabs = (TabHost) findViewById(R.id.tabHost);
         tabs.setup();
         tabs.setCurrentTab(0);
+
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener(){
+            @Override
+            public void onTabChanged(String tabId) {
+//                this.iCurrentTab = tabId
+//                if(TAB_1_TAG.equals(tabId)) {
+//                    //destroy earth
+//                }
+//                if(TAB_2_TAG.equals(tabId)) {
+//                    //destroy mars
+//                }
+            }});
+
+        //oTP.getHour();
+        //oTP.getMinute();
+
 
         for (int i = 0; i < 5; i++) {
             TabHost.TabSpec spec1 = tabs.newTabSpec("tag"+i);
@@ -29,12 +50,26 @@ public class MainActivity extends AppCompatActivity {
                     View wv = getLayoutInflater().inflate(R.layout.activity_tab, tabs.getTabContentView(), false);
                     //you can change tab content here
                     ((Button) wv.findViewById(R.id.button)).setText("Button "+tag);
+                    TimePicker oTP = (TimePicker) wv.findViewById(R.id.timePicker);
+                    oTP.setIs24HourView(true);
+                    oTP.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                        public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                            updateDisplay(hourOfDay, minute);
+                        }
+                    });
+
                     return(wv);
                 }
             });
             spec1.setIndicator("Tab "+i);
             tabs.addTab(spec1);
         }
+
+    }
+
+    private void updateDisplay(int hourOfDay, int minute) {
+
+        this.iCurrentTab = tabs.getCurrentTab();
 
     }
 }
